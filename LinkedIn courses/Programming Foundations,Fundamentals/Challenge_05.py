7548393425324e6d40baf99f9408640e45ac24b0
Abdullah Abubraik <abdallah2000201@gmail.com>
2022-12-10 01:22:56 +0200

    CH 05

---
+++ LinkedIn courses/Programming Foundations,Fundamentals/Challenge_05.py
@@ -0,0 +1,8 @@
+########################## Answer ########################
+def favorite_city(name):
+    print("One of my favorite cities is",name)
+
+favorite_city("Assiut")
+favorite_city("Sewa")
+favorite_city("Luxor")
+###########################################################
--- LinkedIn courses/Programming Foundations,Fundamentals/playground.py
+++ LinkedIn courses/Programming Foundations,Fundamentals/playground.py
@@ -90,4 +90,41 @@
 
 # print("Thanks")
 
+################################ CH 5 ################################
 
+# print("~~~ The Shimmy ~~~")
+# def shimmy():
+#     print("Take one step to the right and stomp.")
+#     print("Take one step to the left and stomp.")
+#     print("Shake those hips!") 
+# shimmy()
+# shimmy()
+
+
+# def hello():
+#     print("Hello, friend!")
+# hello()
+# hello()
+
+
+# def wash_car(amount_paid):
+#     if (amount_paid == 12):
+#         print("Wash with tri-color foam")
+#         print("Rinse twice")
+#         print("Dry with large blow dryer")
+#     if(amount_paid == 6):
+#         print("Wash with white foam")
+#         print("Rinse once")
+#         print("Air dry") 
+# wash_car(12)
+
+
+def withdraw_money(current_balance, amount):
+    if (current_balance >= amount):
+        current_balance = current_balance - amount
+        return current_balance
+balance = withdraw_money(150, 80)
+if balance <=50:
+    print("We need to deposit")
+else:
+    print("You're good")
