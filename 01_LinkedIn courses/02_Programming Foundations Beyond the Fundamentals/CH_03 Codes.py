7bbdce48076d3d3791f6f8e755b0685f0a911c71
Abdullah Abubraik <abdallah2000201@gmail.com>
2022-12-10 03:44:39 +0200

    CH 3

---
+++ 01_LinkedIn courses/02_Programming Foundations Beyond the Fundamentals/CH_03 Codes.py
@@ -0,0 +1,16 @@
+###################################### CH 3 ######################################
+#Module
+#Python file that contains code
+
+#Package or Library
+#Using multiple modules together
+
+#Framework
+#A set of code used in a specific way
+
+import testmodule
+
+testmodule.mult(10, 5)
+
+#A library gives you a set of tools that you can use as you wish.
+#A framework essentially defines how you should accomplish a task.
