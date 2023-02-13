8a0a5adec803db9c37b7add2f0ebf6cfcb17df96
Abdullah Abubraik <abdallah2000201@gmail.com>
2023-02-14 00:27:45 +0200

    module 4 done

--- 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,55 +3,193 @@ const STRONG_ATTACK_VALUE = 18;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 16;
 
-let chosenMaxLife = 100;
+const MODE_ATTACK = "ATTACK";
+const MODE_STRONG_ATTACK = "STRONG_ATTACK";
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
+
+let battleLog = [];
+
+function getMaxLifeValues() {
+  const enteredValue = prompt("Maximum life for you and the monster.", "100");
+
+  const parsedValue = parseInt(enteredValue);
+
+  if (isNaN(parsedValue) || chosenMaxLife <= 0) {
+    throw { message: "Invalid user input, not a number" };
+  }
+
+  return parsedValue;
+}
+
+let chosenMaxLife;
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
+  chosenMaxLife = 100;
+  alert("Invalid user input, default vlaue of 100 was set.");
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+
+  resetGame(chosenMaxLife);
+}
+
+function writeToLog(event, value, monsterHealth, playerHealth) {
+  let logEntry = {
+    event: event,
+    value: value,
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth,
+  };
+
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = "PLAYER";
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = "PLAYER";
+      break;
+  }
+
+  // if (event === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = "MONSTER";
+  // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry.target = "MONSTER";
+  // } else if (event == LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry.target = "PLAYER";
+  // } else if (LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry.target = "PLAYER";
+  // }
+  battleLog.push(logEntry);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+  writeToLog(
+    LOG_EVENT_MONSTER_ATTACK,
+    playerDamage,
+    currentMonsterHealth,
+    currentPlayerHealth
+  );
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert("you would be dead but the bonus life saved you!");
+    return;
+  }
+
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("YOU WON!");
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      "PLAYER_WON",
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert("YOU LOST!");
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0)
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      "MONSTER_WON",
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("DRAW!");
+    writeToLog(
+      LOG_EVENT_GAME_OVER,
+      "DRAW",
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
+  }
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) reset();
 }
 
 function attackMonster(mode) {
-  let maxDamage;
-  if (mode === "ATTACK") {
-    maxDamage = ATTACK_VALUE;
-  } else {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
+  let maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+  let logEvent =
+    mode === MODE_ATTACK
+      ? LOG_EVENT_PLAYER_ATTACK
+      : LOG_EVENT_PLAYER_STRONG_ATTACK;
+  // if (mode === MODE_ATTACK) {
+  //   maxDamage = ATTACK_VALUE;
+  //   logEvent = LOG_EVENT_PLAYER_ATTACK;
+  // } else {
+  //   maxDamage = STRONG_ATTACK_VALUE;
+  //   logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
+  // }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
+  writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
   endRound();
 }
 
 function attackHandler() {
-  attackMonster("ATTACK");
+  attackMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  attackMonster("STRONG_ATTACK");
+  attackMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
   let healValue;
   if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
     alert("You can't heal to more than your max health.");
-    healValue = chosenMaxLife - currentMonsterHealth;
+    healValue = chosenMaxLife - currentPlayerHealth;
   } else {
     healValue = HEAL_VALUE;
   }
   increasePlayerHealth(healValue);
   currentPlayerHealth += healValue;
+  writeToLog(
+    LOG_EVENT_PLAYER_HEAL,
+    healValue,
+    currentMonsterHealth,
+    currentPlayerHealth
+  );
   endRound();
+}
+
+function printLogHandler() {
+  // console.log(battleLog);
+  let i = 0;
+  for (const it of battleLog) {
+    // console.log(it);
+    console.log(`#${i}`);
+    for (const key in it) {
+      console.log(`${key} => ${it[key]}`);
+    }
+    i++;
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHandler);
+logBtn.addEventListener("click", printLogHandler);
---
+++ 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/control-03-monster-killer-starting-project/control-assignment-problem/assignment.js
@@ -0,0 +1,31 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+if (randomNumber >= 0.7)
+  alert(`randomNumber is greater than 0.7  (${randomNumber})`);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+console.log("----------------------");
+
+for (const ele of arr) {
+  console.log(ele);
+}
+
+console.log("----------------------");
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
+console.log("----------------------");
+
+const Random = Math.random();
+
+if (
+  (randomNumber > 0.7 && Random > 0.7) ||
+  randomNumber <= 0.2 ||
+  Random <= 0.2
+)
+  alert(`randomNumber and Random is  (${randomNumber},${Random})`);
