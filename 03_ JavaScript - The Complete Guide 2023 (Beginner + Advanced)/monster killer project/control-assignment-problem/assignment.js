edcdcea991cdc63ce02b143a0b0cf6300c93526c
Abdullah Abubraik <abdallah2000201@gmail.com>
2023-02-14 00:31:27 +0200

    t

--- 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/monster killer project/control-assignment-problem/assignment.js
+++ 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/monster killer project/control-assignment-problem/assignment.js
@@ -1,6 +1,6 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
 if (randomNumber >= 0.7)
-  alert(`randomNumber is greater than 0.7  (${randomNumber})`);
+  alert(`randomNumber is greater than 0.7  =>${randomNumber}`);
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
