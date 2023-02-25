eb40ed7390f5af9602dfaa1ef257db14031fa564
Abdullah Abubraik <abdallah2000201@gmail.com>
2023-02-25 03:05:21 +0200

    js

---
+++ 03_ JavaScript - The Complete Guide 2023 (Beginner + Advanced)/Assignments/dom-assignment-start/assignment.js
@@ -0,0 +1,9 @@
+const way1 = document.querySelector("#task-1");
+const way2 = document.getElementById("task-1");
+way1.style.color = "white";
+way2.style.backgroundColor = "black";
+
+document.querySelector("title").textContent = "Assignment - Solved!";
+document.head.querySelector("title").textContent = "Assignment - Solved!";
+
+document.getElementsByTagName("h1")[0].textContent = "Assignment - Solved!";
